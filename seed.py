3295539a0b2ae418a9470e7cb0a0861c1116e5a3
Kitt <105902506+Kitt630GT@users.noreply.github.com>
2026-05-06 20:14:42 +0700

    เพิ่มข้อมูลเข้า

--- seed.py
+++ seed.py
@@ -24,13 +24,14 @@ def run_seed():
 # ================= Test =================
     # p1 = Product(name='Toyota Fortuner', license_plate='กข-1234', price_per_day=2500, category_id=cat_suv.id, status='available')
     # p2 = Product(name='Honda Civic', license_plate='รย-999', price_per_day=1500, category_id=cat_sedan.id, status='available')
-    # p3 = Product(name='Toyota Alphard', license_plate='ตต-8888', price_per_day=4500, category_id=cat_van.id, status='available')
+    p3 = Product(name='Toyota Alphard', license_plate='ตต-8888', price_per_day=4500, category_id=cat_van.id, status='available')
     # p4 = Product(name='BMW 5 Series', license_plate='หรู-1', price_per_day=5500, category_id=cat_luxury.id, status='available')
     # p5 = Product(name='Porsche 911 Carrera', license_plate='สป-911', price_per_day=12000, category_id=cat_sport.id, status='available')
     # p6 = Product(name='Mazda 2', license_plate='งง-555', price_per_day=1000, category_id=cat_sedan.id, status='available')
     
     # db.add_all([p1, p2, p3, p4, p5, p6])
-    # db.commit()
+    db.add_all([p3])
+    db.commit()
 # ================= END_Test =================
 
     print("Seeding Users...")
@@ -41,22 +42,8 @@ def run_seed():
         
         password='1234'
     )
+    
     u1 = User(
-        fullname='สมชาย โครตซิ่ง', 
-        email='somchai@email.com', 
-        phone='0812345678',
-        
-        password='1234'
-    )
-    u2 = User(
-        fullname='สมศรี สตรีทอง', 
-        email='somsri@email.com', 
-        phone='0898765432',
-        
-        password='1234'
-    )
-
-    u3 = User(
         fullname='Kitt_Kitn', 
         email='68112444@dpu.ac.th', 
         phone='0981234567',
@@ -64,7 +51,7 @@ def run_seed():
         password='68112444'
     )
 
-    u4 = User(
+    u2 = User(
         fullname='pan_akarapon', 
         email='68113440@dpu.ac.th', 
         phone='0891234567',
@@ -72,7 +59,7 @@ def run_seed():
         password='68113440'
     )
     
-    db.add_all([u_admin, u1, u2, u3, u4])
+    db.add_all([u_admin, u1, u2,])
     db.commit()
 
     now = datetime.now()
